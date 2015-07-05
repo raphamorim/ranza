aaa1c7a11f26a820265abfd3a6680cb627a2221c
Raphael Amorim <rapha850@gmail.com>
2015-07-05 18:23:52 -0300

    bug fix [not found other js files]

--- src/core/watcher.js
+++ src/core/watcher.js
@@ -6,8 +6,8 @@ function Watcher(path) {
     return new Promises(function(resolve, reject) {
         var root = rooter(path),
             globOptions = {};
-        
-        glob(root + '**/*.js*', globOptions).then(function(files) {
+
+        glob(root + '/**/*.js*', globOptions).then(function(files) {
             return resolve([files, root]);
         });
     });

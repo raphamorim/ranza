31236c669d658bbe86a920550601ea6ba9a1d933
Raphael Amorim <rapha850@gmail.com>
2015-07-10 02:33:03 -0300

    rename for unusedDependency

--- test/status.js
+++ test/status.js
@@ -10,7 +10,7 @@ function fakeCwd(path) {
 }
 
 describe('Status', function() {
-	context('run on project', function() {
+	context('run project', function() {
 		context('without missed requires and undefined dependencies', function() {
 			it('should return only the used dependencies and a empty unused/undefined list', function(done) {
 				fakeCwd('perfectDependencies');
